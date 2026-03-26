2c18cb6ffb81621b74c3df29e0bc543d8fba9d29
hiba-a11 <hibangrafe@gmail.com>
2026-03-26 14:03:06 +0000

    calc

--- tracker/views.py
+++ tracker/views.py
@@ -1,4 +1,8 @@
+# tracker/views.py
 from django.shortcuts import render
 
 def home(request):
     return render(request, 'tracker/home.html')
+
+def calculators(request):
+    return render(request, 'tracker/calc.html')
