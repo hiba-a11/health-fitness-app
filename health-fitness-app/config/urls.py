60a6cc5d008f28e3269a46f750330801f40aa8eb
hiba-a11 <hibangrafe@gmail.com>
2026-03-26 14:07:20 +0000

    Delete health-fitness-app directory

--- health-fitness-app/config/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from tracker import views  # import all views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home, name='home'),  # home page
-    path('', views.calc, name='calc'),  # calculators page
-]
--- health-fitness-app/tracker/views.py
+++
@@ -1,8 +0,0 @@
-# tracker/views.py
-from django.shortcuts import render
-
-def home(request):
-    return render(request, 'tracker/home.html')
-
-def calculators(request):
-    return render(request, 'tracker/calc.html')
